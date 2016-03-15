777d85b45a1bf49660b2e9a0398b3b011638de4e
Jeff Cole <cole.jeff@gmail.com>
2016-03-15 07:39:27 -0700

    Initial commit.

---
+++ TrafficGame/GameScene.swift
@@ -0,0 +1,23 @@
+//
+//  GameScene.swift
+//  TrafficGame
+//
+//  Created by Jeff on 3/15/16.
+//  Copyright (c) 2016 JeffCole Inc. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+  override func didMoveToView(view: SKView) {
+    /* Setup your scene here */
+  }
+  
+  override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    /* Called when a touch begins */
+  }
+  
+  override func update(currentTime: CFTimeInterval) {
+    /* Called before each frame is rendered */
+  }
+}
